7af5ad26f944f4424fb9cd1d7ff64b33d33ce28b
Safwan Shaheer <devorein00@gmail.com>
2021-08-01 21:01:54 +0600

    Added basic text interpolation

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>{{greet}} {{name}}</div>
+  <div v-text="text"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      text: "Hello World",
+      greet: "Hello",
+      name: "devorein"
+    }
   }
 }
 </script>
