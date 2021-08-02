c89f6031761c8f06ebe9353cbd7fc9edb2a7767b
Safwan Shaheer <devorein00@gmail.com>
2021-08-02 20:31:55 +0600

    Basic hello world component

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,20 @@
 <template>
   <div>
-    <input type="text" v-model="movie"/>
-    <input type="text" v-model="user.name"/>
-    <input type="number" v-model.number="user.age"/>
-    <div v-for="(username, index) in usernames" :key="username">
-      <input type="text" @change="onChange(usernames, index, $event)"/>
-    </div>
+    <Greet />
   </div>
 </template>
 
 <script>
+import Greet from "./components/Greet.vue"
 
 export default {
-  methods: {
-    onChange(usernames, index, event){
-      usernames[index] = event.target.value
-    }
+  components: {
+    Greet
   },
   name: 'App',
   data(){
     return {
-      movie: '',
-      user: {
-        name: "Safwan Shaheer",
-        age: 20
-      },
-      usernames: ["devorein", "arcoden"]
-    }
-  },
-  computed: {
-  },
-  watch: {
-    movie: {
-      handler(newValue){
-        console.log(`Calling api with the movie name ${newValue}`)
-      },
-      immediate: true
-    },
-    user: {
-      handler(newValue){
-        console.log(`Calling api for the user with name ${newValue.name} and age ${newValue.age}`)
-      },
-      deep: true,
-      immediate: true
-    },
-    usernames: {
-      handler(newValue){
-        console.log(`Calling api for the user with new usernames ${newValue}`)
-      },
-      deep: true,
-      immediate: true
+      
     }
   }
 }
---
+++ src/components/Greet.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Hello World
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Greet"
+  }
+</script>
+
+<style scoped>
+
+</style>
