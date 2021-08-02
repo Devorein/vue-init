c96327e8add9beeb62eb4790d7218c33bdadf32d
Safwan Shaheer <devorein00@gmail.com>
2021-08-02 20:47:23 +0600

    Added type, required and default keys in props

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@
     <div v-for="user in users" :key="user.username">
       <Greet :username="user.username" :fullname="user.fullname"/>
     </div>
+    <Article title="Article Title" :isPublished="true"/>
   </div>
 </template>
 
 <script>
 import Greet from "./components/Greet.vue"
+import Article from "./components/Article.vue"
 
 export default {
   components: {
-    Greet
+    Greet,
+    Article
   },
   name: 'App',
   data(){
---
+++ src/components/Article.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>{{title}}</h2>
+    <p>{{likes}}</p>
+    <p>{{isPublished ? "Published" : "Private"}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Article',
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+      likes: {
+        type: Number,
+        default: 0
+      },
+      isPublished: Boolean
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
