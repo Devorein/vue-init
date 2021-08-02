66b0b53c011e1baba503910123645a56470dacba
Safwan Shaheer <devorein00@gmail.com>
2021-08-02 19:15:34 +0600

    Added computed properties and method comparision

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
-    <h2 v-once>{{name}}</h2>
-    <button @click="name = 'Arcoden'">Change name</button>
-    <h2 v-pre>{{name}}</h2>
+    <h2>Firstname {{firstname}}</h2>
+    <h2>Lastname {{lastname}}</h2>
+    <h2>Computed fullname {{fullname}}</h2>
+    <h2>{{ages.reduce((cur, next)=>cur+next, 0)}}</h2>
+    <h2>Computed age {{computedAge}}</h2>
+    <h2>Calculated age {{getTotalAge()}}</h2>
+    <button @click="ages.push(25)">Add item</button>
+    <button @click="firstname='firstname'">Change firstname</button>
   </div>
 </template>
 
@@ -10,11 +15,26 @@
 
 export default {
   methods: {
+    getTotalAge(){
+      console.log("method")
+      return this.ages.reduce((cur, next)=>cur+next, 0)
+    }
   },
   name: 'App',
   data(){
     return {
-      name: "devorein"
+      ages: [20, 31, 44, 13],
+      firstname: "Safwan",
+      lastname: "Shaheer"
+    }
+  },
+  computed: {
+    computedAge(){
+      console.log("computed")
+      return this.ages.reduce((cur, next)=>cur+next, 0)
+    },
+    fullname(){
+      return `${this.firstname} ${this.lastname}`
     }
   }
 }
