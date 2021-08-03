a9f2e11c67aedb992ee2d7ce30a84b47f05d427d
Safwan Shaheer <devorein00@gmail.com>
2021-08-03 11:09:39 +0600

    Added List component

---
+++ src/components/List.vue
@@ -0,0 +1,15 @@
+<template>
+  <h4 v-for="item in items" :key="item.id">
+    <slot :item="item"></slot>
+  </h4>
+</template>
+
+<script>
+  export default {
+    name: 'List',
+  }
+</script>
+
+<style scoped>
+
+</style>
