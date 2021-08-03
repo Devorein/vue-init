4516731066326d9067988647731dd7fd239408c7
Safwan Shaheer <devorein00@gmail.com>
2021-08-03 11:32:15 +0600

    Added Button base component

---
+++ src/components/base/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="$emit('click')">
+    {{label}}
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'Button',
+    props: ['label']
+  }
+</script>
+
+<style scoped>
+  button{
+    background-color: #275EFE;
+    color: white;
+    padding: 3px 10px;
+    margin: 5px;
+    border-radius: 3px;
+  }
+</style>
